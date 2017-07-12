4fefe75f8643344ae59a5e4009c8bf99c59b709b
Luke Wang <lukewang.wyl@gmail.com>
2017-07-12 20:56:55 +0800

    Switch Karabiner profile upon external keyboard plug in / out.

--- config/hammerspoon/private/usb-keyboard.lua
+++ config/hammerspoon/private/usb-keyboard.lua
@@ -12,12 +12,29 @@ function isExternalKeyboard(data)
   return isExternal
 end
 
+function selectKarabinerProfile(idx)
+  hs.osascript.applescript([[
+    tell application "System Events" to tell process "Karabiner-Menu"
+      ignoring application responses
+        click menu bar item 1 of menu bar 1
+      end ignoring
+    end tell
+    do shell script "killall System\\ Events"
+    delay 0.1
+    tell application "System Events" to tell process "Karabiner-Menu"
+      click menu item ]] .. (idx + 1) .. [[ of menu 1 of menu bar item 1 of menu bar 1
+    end tell
+  ]])
+end
+
 function usbDeviceCallback(data)
   if (isExternalKeyboard(data)) then
     if (data.eventType == "added") then
       hs.alert.show("External keyboard added")
+      selectKarabinerProfile(1)
     elseif (data.eventType == "removed") then
       hs.alert.show("External keyboard removed")
+      selectKarabinerProfile(0)
     end
   end
 end
@@ -29,7 +46,8 @@ for i, usbDevice in pairs(usbTable) do
     hasExternal = true
   end
 end
-hs.alert.show("External keyboard "..(hasExternal and "available" or "unavailable"))
+hs.alert.show("External keyboard "..(hasExternal and "connected" or "not exists"))
+selectKarabinerProfile(hasExternal and 1 or 0)
 
 local usbWatcher = hs.usb.watcher.new(usbDeviceCallback)
 usbWatcher:start()
